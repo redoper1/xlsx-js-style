de8c3d1cb4fe799866b890841aeeabdd018ac281
David Rychlý <david@davidrychly.cz>
2024-10-16 17:29:46 +0200

    updated for 1.2.1

--- gulpfile.js
+++ gulpfile.js
@@ -23,10 +23,20 @@ gulp.task("min", () => {
 		.pipe(gulp.dest("./dist/"));
 });
 
+gulp.task("full", () => {
+	return gulp
+		.src(["./src/xlsx.js"])
+		.pipe(concat("xlsx.js"))
+		.pipe(replace("./dist/cpexcel.js", "./cpexcel.js"))
+		.pipe(insert.prepend("/* xlsx-js-style " + pkg.version + " @ " + new Date().toISOString() + " */\n"))
+		.pipe(ignore.exclude(["**/*.map"]))
+		.pipe(gulp.dest("./dist/"));
+});
+
 gulp.task("bundle", () => {
 	return gulp
 		.src(["./libs/*", "./src/xlsx.js"])
-		.pipe(concat("xlsx.bundle.js"))
+		.pipe(concat("xlsx.bundle.min.js"))
 		.pipe(uglify())
 		.pipe(replace("./dist/cpexcel.js", "./cpexcel.js"))
 		.pipe(insert.prepend("/* xlsx-js-style " + pkg.version + " @ " + new Date().toISOString() + " */\n"))
@@ -36,11 +46,27 @@ gulp.task("bundle", () => {
 		.pipe(gulp.dest("./dist/"));
 });
 
+gulp.task("bundleFull", () => {
+	return gulp
+		.src(["./libs/*", "./src/xlsx.js"])
+		.pipe(concat("xlsx.bundle.js"))
+		.pipe(replace("./dist/cpexcel.js", "./cpexcel.js"))
+		.pipe(insert.prepend("/* xlsx-js-style " + pkg.version + " @ " + new Date().toISOString() + " */\n"))
+		.pipe(ignore.exclude(["**/*.map"]))
+		.pipe(gulp.dest("./dist/"));
+});
+
 gulp.task("nodeTest", () => {
 	return gulp.src(["./dist/xlsx.min.js"]).pipe(gulp.dest("./demos/node/node_modules/xlsx-js-style/dist"));
 });
 
 // Build/Deploy (ad-hoc, no watch)
-gulp.task("ship", gulp.series("min", "bundle", "nodeTest"), () => {
+gulp.task("ship", gulp.series("full", "min", "bundle", "bundleFull", "nodeTest"), () => {
 	console.log("... ./dist/*.js files created!");
 });
+
+gulp.task("watch", () => {
+	gulp.watch(["./src/xlsx.js", "./libs/*.js"], gulp.series("ship"));
+});
+
+gulp.task("default", gulp.series("watch"));
