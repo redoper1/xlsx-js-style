70842a676eccfc152586a00a59562aeb5a501fc2
David Rychlý <david@davidrychly.cz>
2024-11-26 20:24:09 +0100

    test.ts small update

--- tests/test.ts
+++ tests/test.ts
@@ -6,8 +6,8 @@ import * as XLSX from "xlsx-js-style";
 
 //const wb:XLSX.WorkBook = []
 const wb = XLSX.utils.book_new();
-let row1: XLSX.CellObject[] = ["a", "b", "c"];
-let row2: XLSX.CellObject[] = [1, 2, 3];
+let row1: XLSX.CellObject[] = [{ v: "a", t: "s" }, { v: "b", t: "s" }, { v: "c", t: "s" }]; //["a", "b", "c"];
+let row2: XLSX.CellObject[] = [{ v: 1, t: "n" }, { v: 2, t: "n" }, { v: 3, t: "n" }]; //[1, 2, 3];
 
 const ws = XLSX.utils.aoa_to_sheet([row1, row2]);
 XLSX.utils.book_append_sheet(wb, ws, "SheetJS");
